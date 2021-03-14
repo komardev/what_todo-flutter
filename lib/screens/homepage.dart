689512b6aaccdd3a645c61206f047fcee2f11973
komar <komar@it.damcorp.id>
2021-03-14 22:03:28 +0700

    finish ui

---
+++ lib/screens/homepage.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_todo_app/screens/taskpage.dart';
+import 'package:flutter_todo_app/widgets.dart';
+
+class Homepage extends StatefulWidget {
+  @override
+  _HomepageState createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          width: double.infinity,
+          padding: EdgeInsets.symmetric(
+            horizontal: 24.0,
+          ),
+          color: Color(0xFFF6F6F6),
+          child: Stack(
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    margin: EdgeInsets.only(
+                      top: 32.0,
+                      bottom: 32.0
+                    ),
+                    child: Image(
+                      image: AssetImage(
+                        'assets/images/logo.png'
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: ScrollConfiguration(
+                      behavior: NoGlowBehaviour(),
+                      child: ListView(
+                        children: [
+                          TaskCardWidget(
+                            title: "Get Started!",
+                            desc: "Hello User! Welcome to WHAT_TODO app, this is a default task that you can edit or delete to start using the app.",
+                          ),
+                          TaskCardWidget(),
+                          TaskCardWidget(),
+                          TaskCardWidget(),
+                          TaskCardWidget(),
+                          TaskCardWidget(),
+                          TaskCardWidget(),
+                          TaskCardWidget(),
+                        ],
+                      ),
+                    ),
+                  )
+                ],
+              ),
+              Positioned(
+                bottom: 25.0,
+                right: 0.0,
+                child: GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                      context, 
+                      MaterialPageRoute(
+                        builder: (context) => Taskpage()
+                      )
+                    );
+                  },
+                  child: Container(
+                    width: 60.0,
+                    height: 60.0,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(50.0),
+                      gradient: LinearGradient(
+                        colors: [Color(0xFF7349FE), Color(0xFFA78CFC)],
+                        begin: Alignment(0.0, -1.0),
+                        end: Alignment(0.0, 1.0)
+                      )
+                      // color: Color(0xFF7349FE)
+                    ),
+                    child: Image(image: AssetImage(
+                      "assets/images/add_icon.png"
+                    )),
+                  ),
+                ),
+              )
+            ],
+          )
+        ),
+      ),
+    );
+  }
+}
